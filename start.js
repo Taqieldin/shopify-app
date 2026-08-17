bbe12673dd1f701867ce19b108c2b8dab01b0336
shouka23 <Shouka23@yahoo.com>
2026-08-17 23:58:14 +0300

    Fix start.js to spawn npx tsx as child process

--- start.js
+++ start.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import { execSync } from "child_process";
+import { chdir } from "process";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-process.chdir(__dirname);
-
+chdir(__dirname);
 execSync("npx tsx server/index.ts", { stdio: "inherit" });
