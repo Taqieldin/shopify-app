efd70969d5aa5b604dc3b4654acff5e684c851af
shouka23 <Shouka23@yahoo.com>
2026-08-17 23:53:13 +0300

    Fix start.js for Node 19 compatibility

--- start.js
+++ start.js
@@ -1,3 +1,9 @@
-import "dotenv/config";
-import "tsx/esm";
-import "./server/index.ts";
+#!/usr/bin/env node
+import { execSync } from "child_process";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+process.chdir(__dirname);
+
+execSync("npx tsx server/index.ts", { stdio: "inherit" });
