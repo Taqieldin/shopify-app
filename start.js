45d670b40e5ac9f69c14eda547e6536c310bc098
shouka23 <Shouka23@yahoo.com>
2026-08-18 02:09:37 +0300

    Fix start.js for cPanel Passenger with Node 22

--- start.js
+++ start.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-import { execSync } from "child_process";
-import { chdir } from "process";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { register } from "node:module";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-chdir(__dirname);
-execSync("npx tsx server/index.ts", { stdio: "inherit" });
+process.chdir(__dirname);
+
+register("tsx/esm", import.meta.url);
+
+await import("./server/index.ts");
